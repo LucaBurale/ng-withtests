bb50a5ee58332ee31e322d972d57bdcddb8ea1db
lucaburale <luca.burale@S2E-mail.com>
2021-11-24 16:36:20 +0100

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -26,10 +27,19 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng-withtests');
   });
 
+  // test titolo
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-withtests app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Test di Luca');
+  });
+
+  // test paragrafo
+  it('should render paragraph', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('p')?.textContent).toContain('Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eveniet vitae nostrum architecto. Omnis maxime ipsam laboriosam commodi nobis reprehenderit? Est totam deserunt recusandae, earum quibusdam nisi distinctio natus quos.');
   });
 });
